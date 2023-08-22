5c99e18e59cd45ff91d5820f31bd5c068f4765ca
원준 <92675559+JUNE06-3723684484@users.noreply.github.com>
2023-08-22 20:13:49 +0900

    create graph

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const board = JXG.JSXGraph.initBoard(box, {
+    boundingbox: [-20, 20, 20, -20],
+    axis: true
+});
+
+let a = board.create('slider', [[3, -5], [13, -5], [-20, 0, 20]], {name: 'a'});
+let b = board.create('slider', [[3, -7], [13, -7], [-20, 0, 20]], {name: 'b'});
+
+let postiveGraph = board.create('functiongraph', [function(x) {
+    return Math.sqrt(Math.pow(x, 3) + a.Value() * x + b.Value());
+}], {strokeWidth: 3, strokeColor: 'black'});
+
+let negativeGraph = board.create('functiongraph', [function(x) {
+    return -Math.sqrt(Mate.pow(x, 3) + a.Value() * x + b.Value());
+}], {strokeWidth: 3, strokeColor: 'black'});
