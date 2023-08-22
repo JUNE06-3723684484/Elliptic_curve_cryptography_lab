44efbf1f3f00ebf3a28755ea4437d5a2bce019f3
원준 <92675559+JUNE06-3723684484@users.noreply.github.com>
2023-08-22 20:32:48 +0900

    add comma

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-board = JXG.JSXGraph.initBoard(box, {
+const board = JXG.JSXGraph.initBoard('box', {
     boundingbox: [-20, 20, 20, -20],
     axis: true
 });
 
-let a = board.create('slider', [[3, -5], [13, -5], [-20, 0, 20]], {name: 'a'});
-let b = board.create('slider', [[3, -7], [13, -7], [-20, 0, 20]], {name: 'b'});
+let a = board.createElement('slider', [[3, -5], [13, -5], [-20, 0, 20]], {name: 'a'});
+let b = board.createElement('slider', [[3, -7], [13, -7], [-20, 0, 20]], {name: 'b'});
 
-let postiveGraph = board.create('functiongraph', [function(x) {
+let postiveGraph = board.createElement('functiongraph', [function(x) {
     return Math.sqrt(Math.pow(x, 3) + a.Value() * x + b.Value());
 }], {strokeWidth: 3, strokeColor: 'black'});
 
-let negativeGraph = board.create('functiongraph', [function(x) {
+let negativeGraph = board.createElement('functiongraph', [function(x) {
     return -Math.sqrt(Mate.pow(x, 3) + a.Value() * x + b.Value());
 }], {strokeWidth: 3, strokeColor: 'black'});
